f5989bd8e0d54971b624423717d6b990c4f8b824
Manish Singh <instrido@gmail.com>
2022-06-30 02:08:07 +0530

    Solve part 1 - flattening an array

---
+++ src/flatten.ts
@@ -0,0 +1,17 @@
+// Part 1 – Flattening an array
+export const getFlattenedArray = <T>(nestedArray: unknown): T[] => {
+  if (!Array.isArray(nestedArray)) {
+    throw new Error('Parameter is not an array.');
+  }
+
+  let flattenedArray: T[] = [];
+  for (const element of nestedArray as T[]) {
+    if (Array.isArray(element)) {
+      flattenedArray = flattenedArray.concat(getFlattenedArray(element));
+    } else {
+      flattenedArray.push(element);
+    }
+  }
+
+  return flattenedArray;
+};
