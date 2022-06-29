9bf5b7f14359ac7a4ba7e517e767bed3394bf261
Manish Singh <instrido@gmail.com>
2022-06-30 02:26:58 +0530

    Improve coverage.

--- tests/flatten.test.ts
+++ tests/flatten.test.ts
@@ -1,8 +1,14 @@
 import {getFlattenedArray} from '../src/flatten';
 
 test('getFlattenedArray function', () => {
+  expect(getFlattenedArray([1, 2, 3])).toEqual([1, 2, 3]);
   expect(getFlattenedArray([1, [2, [3]], 4])).toEqual([1, 2, 3, 4]);
   expect(getFlattenedArray([1, [2, [3]], 4, [5, [6]]])).toEqual([
     1, 2, 3, 4, 5, 6,
   ]);
+  expect(() => getFlattenedArray(1)).toThrow(TypeError);
+  expect(() => getFlattenedArray(1)).toThrow('Parameter is not an array.');
+  expect(() => getFlattenedArray('')).toThrowError(
+    'Parameter is not an array.'
+  );
 });
